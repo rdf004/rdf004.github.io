57530af2136af3e44aeb8189c6256188c30cee76
Roshan Fernando <roshan@Roshans-MacBook-Pro.local>
2019-09-14 14:17:22 -0700

    Updates

---
+++ precache-manifest.a36ab82cab7f3a91e084a87653b56899.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b681f886110c8e2dc8e502df89a88d4",
+    "url": "/rdf004.github.io/index.html"
+  },
+  {
+    "revision": "396648d0508f0a309546",
+    "url": "/rdf004.github.io/static/css/main.fc1b2b40.chunk.css"
+  },
+  {
+    "revision": "c0bd8d06e42f8b80759d",
+    "url": "/rdf004.github.io/static/js/2.c0218ba0.chunk.js"
+  },
+  {
+    "revision": "396648d0508f0a309546",
+    "url": "/rdf004.github.io/static/js/main.411a8b11.chunk.js"
+  },
+  {
+    "revision": "54f8a1b0134475bd969f",
+    "url": "/rdf004.github.io/static/js/runtime~main.963ef5b7.js"
+  },
+  {
+    "revision": "066c8b7ceb0b748172fa4ec9b689d631",
+    "url": "/rdf004.github.io/static/media/SF-Pro-Display-Heavy.066c8b7c.otf"
+  },
+  {
+    "revision": "ae18f7676c12ce1ebe8109ac78bb3f3f",
+    "url": "/rdf004.github.io/static/media/SF-Pro-Display-Regular.ae18f767.otf"
+  },
+  {
+    "revision": "0d61db61d7adaefb9b0c4b0b2e979116",
+    "url": "/rdf004.github.io/static/media/SF-Pro-Display-Thin.0d61db61.otf"
+  }
+]);
