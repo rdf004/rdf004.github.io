1734ac2b8985d9432eeec426a94dc83caae69c48
Roshan Fernando <roshan@Roshans-MacBook-Pro.local>
2019-09-14 14:38:26 -0700

    Updates

---
+++ precache-manifest.51c0428609730b1b760c1c396bf5caf3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ee59fa8dc34b495ca34e9a2ab19f420",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92dba90304f265e8b294",
+    "url": "/static/css/main.22cd0624.chunk.css"
+  },
+  {
+    "revision": "c0bd8d06e42f8b80759d",
+    "url": "/static/js/2.c0218ba0.chunk.js"
+  },
+  {
+    "revision": "92dba90304f265e8b294",
+    "url": "/static/js/main.876ad5a5.chunk.js"
+  },
+  {
+    "revision": "ca5c8fe1c991f76257d4",
+    "url": "/static/js/runtime~main.105f67c5.js"
+  },
+  {
+    "revision": "066c8b7ceb0b748172fa4ec9b689d631",
+    "url": "/static/media/SF-Pro-Display-Heavy.066c8b7c.otf"
+  },
+  {
+    "revision": "ae18f7676c12ce1ebe8109ac78bb3f3f",
+    "url": "/static/media/SF-Pro-Display-Regular.ae18f767.otf"
+  },
+  {
+    "revision": "0d61db61d7adaefb9b0c4b0b2e979116",
+    "url": "/static/media/SF-Pro-Display-Thin.0d61db61.otf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rdf004.github.io/precache-manifest.a36ab82cab7f3a91e084a87653b56899.js"
+  "/precache-manifest.51c0428609730b1b760c1c396bf5caf3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rdf004.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
